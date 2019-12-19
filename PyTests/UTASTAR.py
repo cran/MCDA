be856a5c0dfe0a10fd30ca524bedc525e17e6155
Alexandros Michael <37590485+AlexandrosMariosMichael@users.noreply.github.com>
2019-12-19 18:37:06 +0200

    Moved tests to this file

--- PyTests/UTASTAR.py
+++ PyTests/UTASTAR.py
@@ -1,8 +1,6 @@
-import pandas as pd
-from utastarpy import UTASTAR
+from Python.UTASTAR import UTASTAR
 
-# import numpy as np
-
+# UTASTAR EXAMPLE
 # the separation threshold
 
 epsilon = 0.05
@@ -15,47 +13,84 @@ performanceTable = pd.DataFrame(
     index=["RER", "METRO1", "METRO2", "BUS", "TAXI"],
 )
 
-print(performanceTable,"Performance Table")
+print("\nPerformance Table \n", performanceTable)
 # ranks of the alternatives
 data = [1, 2, 2, 3, 4]
 rownames = performanceTable.index.values
-alternativesRanks = pd.DataFrame(
-   [data] , columns=rownames
-)
-print(alternativesRanks,"Alternative Ranks")
+alternativesRanks = pd.DataFrame([data], columns=rownames)
+print("\nAlternative Ranks\n", alternativesRanks)
 
 # criteria to minimize or maximize
 data = ["min", "min", "max"]
 columnnames = performanceTable.columns.values
-criteriaMinMax = pd.DataFrame(
-    [data], columns=columnnames
+criteriaMinMax = pd.DataFrame([data], columns=columnnames)
+print(
+    "\nCriteria Min Max \n", criteriaMinMax,
 )
-print(criteriaMinMax,"Criteria MIN MAX")
 # number of break points for each criterion
 
 criteriaNumberOfBreakPoints = pd.DataFrame(
     [[3, 4, 4]], columns=performanceTable.columns.values
 )
-print(criteriaNumberOfBreakPoints, " criteriaNumofBP")
+print("\nCriteriaNumofBP\n", criteriaNumberOfBreakPoints)
 
-x1 = UTASTAR(performanceTable, criteriaMinMax, criteriaNumberOfBreakPoints, epsilon, alternativesRanks=alternativesRanks)
 
-#assert(x1["Kendall"] == 1)
+x = UTASTAR(
+    performanceTable,
+    criteriaMinMax,
+    criteriaNumberOfBreakPoints,
+    epsilon,
+    alternativesRanks=alternativesRanks,
+)
+
+print(x)
+
+x1 = UTASTAR(
+    performanceTable,
+    criteriaMinMax,
+    criteriaNumberOfBreakPoints,
+    epsilon,
+    alternativesRanks=alternativesRanks,
+    kPostOptimality=0.5,
+)
+
+print(x1)
+# assert Kendall == 1
 
 # let us try the same with the pairwise preferences to test if the results
 # are the same
 
-alternativesPreferences = pd.DataFrame([["RER", "METRO1"], ["METRO2", "BUS"], ["BUS", "TAXI"]])
+alternativesPreferences = pd.DataFrame(
+    [["RER", "METRO1"], ["METRO2", "BUS"], ["BUS", "TAXI"]]
+)
 print(alternativesPreferences)
-alternativesIndifferences = pd.DataFrame(["METRO1", "METRO2"])
+alternativesIndifferences = pd.DataFrame([["METRO1"], ["METRO2"]])
+alternativesIndifferences = alternativesIndifferences.transpose()
 print(alternativesIndifferences)
 
-x1prime =  UTASTAR(performanceTable, criteriaMinMax, criteriaNumberOfBreakPoints, epsilon,alternativesPreferences=alternativesPreferences, alternativesIndifferences=alternativesIndifferences)
+x1prime = UTASTAR(
+    performanceTable,
+    criteriaMinMax,
+    criteriaNumberOfBreakPoints,
+    epsilon,
+    alternativesPreferences=alternativesPreferences,
+    alternativesIndifferences=alternativesIndifferences,
+)
 
+print(x1prime)
 # assert(all(x1$valueFunctions$Price == x1prime$valueFunctions$Price) & & all(x1$valueFunctions$Time == x1prime$valueFunctions$Time) & & all(x1$valueFunctions$Comfort == x1prime$valueFunctions$Comfort))
 
 # now some filtering
-x2 = UTASTAR(performanceTable, criteriaMinMax, criteriaNumberOfBreakPoints, epsilon,alternativesRanks=alternativesRanks, criteriaIDs=["Price", "Time"], alternativesIDs=["METRO1", "METRO2", "TAXI"])
 
+x2 = UTASTAR(
+    performanceTable,
+    criteriaMinMax,
+    criteriaNumberOfBreakPoints,
+    epsilon,
+    alternativesRanks=alternativesRanks,
+    criteriaIDs=["Price", "Time"],
+    alternativesIDs=["METRO1", "METRO2", "TAXI"],
+)
+
+print(x2)
 # assert(x2$overallValues[1] == x2$overallValues[2])
-

c08ae809532473a8eccf016f9165ef01fcc76bd2
alex_mixail <alex_mixail@hotmail.com>
2019-12-22 23:43:31 +0200

    UTADIS to python first example completed

---
+++ PyTests/UTADIS.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from Python.UTADIS import *
+
+# UTADIS EXAMPLE
+# the separation threshold
+
+epsilon = 0.05
+
+# the performance table
+data = ([3, 10, 1], [4, 20, 2], [2, 20, 0], [6, 40, 0], [30, 30, 3])
+performanceTable = pd.DataFrame(
+    data,
+    columns=["Price", "Time", "Comfort"],
+    index=["RER", "METRO1", "METRO2", "BUS", "TAXI"],
+)
+
+print("\nPerformance Table \n", performanceTable)
+# ranks of the alternatives
+data = ["good", "medium", "medium", "bad", "bad"]
+rownames = performanceTable.index.values
+alternativesAssignments = pd.DataFrame([data], columns=rownames)
+print("\nAlternative Ranks\n", alternativesAssignments)
+
+# criteria to minimize or maximize
+data = ["min", "min", "max"]
+columnnames = performanceTable.columns.values
+criteriaMinMax = pd.DataFrame([data], columns=columnnames)
+print(
+    "\nCriteria Min Max \n", criteriaMinMax,
+)
+# number of break points for each criterion
+
+criteriaNumberOfBreakPoints = pd.DataFrame(
+    [[3, 4, 4]], columns=performanceTable.columns.values
+)
+print("\nCriteriaNumofBP\n", criteriaNumberOfBreakPoints)
+
+
+categoriesRanks = pd.DataFrame([[1, 2, 3]], columns=["good", "medium", "bad"])
+# ranks of the categories
+print("\nCategoriesRanks", categoriesRanks, sep="\n")
+
+(
+        optimum,
+        valueFunctions,
+        overallValues,
+        categoriesLBs,
+        errorValuesPlus,
+        errorValuesMinus,
+        #minWeights,
+        #maxWeights,
+        #averageValueFunctions,
+        #averageOverallValues,
+    ) = UTADIS(
+    performanceTable,
+    criteriaMinMax,
+    criteriaNumberOfBreakPoints,
+    alternativesAssignments,
+    categoriesRanks,
+    0.1,
+)
+
+print(
+        optimum,
+        valueFunctions,
+        overallValues,
+        categoriesLBs,
+        errorValuesPlus,
+        errorValuesMinus,
+        #minWeights,
+        #maxWeights,
+        #averageValueFunctions,
+        #averageOverallValues,
+        sep='\n'
+
+    )
+# filtering out category "good" and assigment examples "RER" and "TAXI"
+
+# y<-UTADIS(performanceTable, criteriaMinMax, criteriaNumberOfBreakPoints, alternativesAssignments, categoriesRanks,0.1, categoriesIDs=c("medium","bad"), alternativesIDs=c("METRO1","METRO2","BUS"))
+
+# # working furthermore on only 2 criteria : "Comfort" and "Time"
+
+# z<-UTADIS(performanceTable, criteriaMinMax, criteriaNumberOfBreakPoints, alternativesAssignments, categoriesRanks,0.1, criteriaIDs=c("Comfort","Time"))
+
+# stopifnot(x$optimum ==0 && y$optimum ==0 && z$optimum ==0)
+

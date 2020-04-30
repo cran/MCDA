4e69500b0e7ecdc2a029f74e30f5b0a6e73ff29d
alex_mixail <alex_mixail@hotmail.com>
2020-04-30 10:10:56 +0300

    ypdates

--- UtastarCreditScore.py
+++ UtastarCreditScore.py
@@ -102,11 +102,7 @@ minmaxdata = ['min',
  'min']
 columnnames = performanceTable.columns.values
 criteriaMinMax = pd.DataFrame([minmaxdata], columns=columnnames)
-print(
-    "\nCriteria Min Max \n", criteriaMinMax,
-)
-
-
+print("\nCriteria Min Max \n", criteriaMinMax,)
 
 bpdata = [4, 3, 3, 4, 4, 4, 3, 4, 2, 3, 3, 3, 4, 4, 3, 2, 4, 4, 3, 3]
 #choices = [2,3,4]
@@ -163,7 +159,7 @@ criteriaNumberOfBreakPoints = pd.DataFrame(
     #     print(bpdata)
     #     print(minmaxdata)
     #     break
-
+""" 
 print(
     optimum,  
     valueFunctions,
@@ -177,11 +173,11 @@ print(
     averageValueFunctions,
     averageOverallValues,
     sep="\n",
-)
+) """
 
 
 
-print("X ends Here")
+#print("X ends Here")
 
 (
     optimum,
@@ -238,8 +234,9 @@ averageOverallValues.to_excel(
     r"E:\Google Drive\PC SHIT\HMMY\Diplomatiki\german credit score dataset UCI\ResultsUtastar\AverageOverallValues.xlsx"
 )
 
-# Insert to multi criteria matrix
 
+
+# Insert to multi criteria matrix
 
 # Insert averageOverallValues as column  to Alternatives
 df.insert(21, "AverageOverallValues", averageOverallValues.transpose())
@@ -248,13 +245,29 @@ df.insert(22, "OverallValues", overallValues.transpose())
 # Inert outRanks values as column to Alternatives
 df.insert(23, "OutRanks", outRanks.transpose())
 
+#Accurancy true postives + false postives 
+nrows= df.shape[0]
+y=df.columns.get_loc("Class")
+y2=df.columns.get_loc("OutRanks")
+accur = [1 for x in range(1, df.shape[0]) if (df.iloc[x,y2]<nrows/2 and df.iloc[x,y] == 1)   ]
+accur2 = [1 for x in range(1, df.shape[0]) if (df.iloc[x,y2]>nrows/2 and df.iloc[x,y]==2) ]
+
+print("Accuracy", sum(accur+accur2))
+
+
+
 # Insert valueFunctions to criteria/columns
 data = [valueFunctions.iloc[x, (bpdata[x//2]-1)] for x in range(1, len(valueFunctions), 2) ]
 data = pd.DataFrame(data, index=performanceTable.columns.values, columns=["ValueFunc"])
-data = data.transpose()
+valuefunc = data.transpose()
 
-# df = df.append(data)
-df = pd.concat([data, df], ignore_index=False)
+#distribute overall values to all dataframe based on valueFunc
+for i in range(0,nrows):
+    margin = df.iloc[i,-2] * valuefunc.values
+    df.iloc[i,0:-4] = df.iloc[i,0:-4] * margin.flatten()
+
+#Append valuefunc row 
+df = pd.concat([valuefunc, df], ignore_index=False)
 
 # Insert averagevalueFunctions to criteria/columns
 data = [
@@ -263,13 +276,15 @@ data = [
 data = pd.DataFrame(
     data, index=performanceTable.columns.values, columns=["AverageValueFunc"]
 )
-data = data.transpose()
-# df = df.append(data)
-df = pd.concat([data, df], ignore_index=False)
+avaluefunc = data.transpose()
+# append average value func row 
+df = pd.concat([avaluefunc, df], ignore_index=False)
 
 
 print(df)
 df.to_excel(
     r"E:\Google Drive\PC SHIT\HMMY\Diplomatiki\german credit score dataset UCI\ResultsUtastar\Results.xlsx"
 )
+
+print("Accuracy", sum(accur+accur2))
 

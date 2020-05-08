7b7f96967e9268376a0f51132d13811c278f9c81
Alexandros Michail <amichail@raycap.gr>
2020-05-08 16:00:38 +0300

    k-means added

--- UtastarCreditScore.py
+++ UtastarCreditScore.py
@@ -475,3 +475,63 @@ pp = sns.pairplot(dfplot[cols], size=2, aspect=1,
 fig = pp.fig 
 fig.subplots_adjust(top=0.93, wspace=0.3)
 t = fig.suptitle('Credit card Pairwise Plots', fontsize=14)
+
+################### k means for feautre/alternatives  reduction ###############
+
+
+
+#3 Using the elbow method to find out the optimal number of #clusters. 
+#KMeans class from the sklearn library.
+from sklearn.cluster import KMeans
+
+wcss=[]
+#dfk = performanceTable.values
+dfk = df.iloc[1:,:-3].values 
+
+for i in range(1,11): 
+    kmeans = KMeans(n_clusters=i, init ='k-means++', max_iter=300,  n_init=10,random_state=0 )
+
+    kmeans.fit(dfk)
+
+    wcss.append(kmeans.inertia_)
+#kmeans inertia_ attribute is:  Sum of squared distances of samples #to their closest cluster center.
+#4.Plot the elbow graph
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method Graph')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+#5 According to the Elbow graph we deterrmine the clusters number as #5. Applying k-means algorithm to the X dataset.
+kmeans = KMeans(n_clusters=4, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
+
+# We are going to use the fit predict method that returns for each #observation which cluster it belongs to. 
+# The cluster to which #it belongs and it will return this cluster numbers into a 
+# # single vector that is  called y K-means
+
+X = dfk
+y_kmeans = kmeans.fit_predict(X)
+
+
+# #6 Visualising the clusters based on prediction 
+plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 1], s=10, c='red', label ='Cluster 1' )
+plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 1], s=10, c='blue', label ='Cluster 2')
+
+
+#visual based on actual values values 
+#plt.scatter(X[:,:10],X[:,10:],alpha = 0.5)
+
+#Plot the centroid. This time we're going to use the cluster centres  
+# attribute that returns here the coordinates of the centroid.
+plt.scatter(kmeans.cluster_centers_[:, :1], kmeans.cluster_centers_[:, 2:3], s=30, c='yellow', label = 'Centroids' )
+plt.title('Clusters k-means')
+#plt.xlim(0.00175,0.0035)# predicted
+#plt.ylim(0.0006,0.0015) #
+
+#plt.xlim(0.00175,0.0035) #actual data 
+#plt.ylim(0.0006,0.0015)
+
+#plt.xlabel('Values)')
+#plt.ylabel('Spending Score(1-100')
+plt.show()
